7c415fb556f862bd3577f5510e9a6f01c60e0b02
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2022-02-13 16:59:11 +0800

    imported json and fpdf, and encodes a code that will open the JSON file

---
+++ JSONReader.py
@@ -0,0 +1,43 @@
+#Assignment 9
+
+#PDF Resume Creator
+#	- Create a python program that will create your personal resume in PDF format
+#	- All personal details are stored in a JSON file
+#	- Your program should read the JSON file and write the details in the PDF
+#	- The output file should be: LASTNAME_FIRSTNAME.pdf
+
+#Note:
+#	- Search for available PDF library that you can use
+#	- Search how data is structured using JSON format
+#	- Search how to read JSON file
+#	- You will create the JSON file manually
+#	- Your code should be in github before Feb12
+
+import json
+from turtle import width
+from fpdf import FPDF
+
+resumeVar = "ResumeInfo.json"
+with open(resumeVar, "r") as JSONFile:
+    ResumeInfo = json.loads(JSONFile.read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
